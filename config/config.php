ab0ade6e75c94d163ef61aa2014ad7e40e52e2b9
LaurenceB28 <laurence.brd@gmail.com>
2023-05-30 16:45:40 +0200

    regex et clean

--- config/config.php
+++ config/config.php
@@ -1,4 +1,8 @@
 <?php
+
 define('DSN','mysql:dbname=hospitalE2N;host=127.0.0.1');
 define('USER',  'cabinet-medical');
 define('PASSWORD',  '8LBjXEG-klmOSRpn');
+
+define('REGEX_NO_NUMBER',"^[A-Za-z-éèêëàâäôöûüç' ]+$");
+define('REGEX_DATE','^([0-9]{4})[\/\-]?([0-9]{2})[\/\-]?([0-9]{2})$');
